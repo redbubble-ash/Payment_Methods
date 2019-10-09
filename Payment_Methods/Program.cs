6b7e33086b69f87d3ed41efaf30ce51b3934af3e
Your Name <xiejing418@gmail.com>
2019-10-08 23:38:02 -0700

    Created a solution

---
+++ Payment_Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Payment_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
